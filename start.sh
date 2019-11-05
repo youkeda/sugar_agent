6025581fa1241f700fd015b7a2a3b982032c526f
jiaoge <yingjunjiao@outlook.com>
2019-11-05 13:51:51 +0800

    fix

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+image=$(docker images | grep 'registry-vpc.cn-hangzhou.aliyuncs.com/youkeda/sugar_agent' | awk '{print $3}')
+if [ -n "$image" ] ; then
+    docker rmi $image
+fi
+
+dockerName=registry-vpc.cn-hangzhou.aliyuncs.com/youkeda/sugar_agent:$1
+
+echo $dockerName
+
+docker pull $dockerName
+
+
+docker run --name suagr_agent_$(TZ=CST-8 date '+%Y%m%d-%H%M%S') -v ~/.m2/repository:/root/.m2/repository  --restart=unless-stopped  -d $dockerName
